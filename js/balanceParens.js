aa88b29c302670cbe38a9684166b6f61ab2bf289
Galuf1 <daniel.pizarro.farias@gmail.com>
2022-05-30 17:44:51 +0200

    js solved with stack

--- js/balanceParens.js
+++ js/balanceParens.js
@@ -1,6 +1,24 @@
 balanceParens = (str) =>
 {
+    let splitted = str.split("")
+    let stack = []
 
+    for (let i = 0; i < str.length; i++) {
+        if (splitted[i] === "("){
+            stack.push(i)
+        } else if ( splitted[i] === ")"){
+            if (stack.length == 0){
+                splitted[i] = ""
+            } else{
+                stack.pop()
+            }
+        }
+    }
+    for (j of stack) {
+        splitted[j] = ""
+    }
+    // console.log(splitted)
+    return splitted.join("")
 }
 
 module.exports = { balanceParens }
--- js/balanceParensSpec.js
+++ js/balanceParensSpec.js
@@ -6,3 +6,20 @@ console.log(balanceParens("abc((d)e(fgh)(i)j(k") === "abc(d)e(fgh)(i)jk")
 
 // Challenge: nested parentheses...
 console.log(balanceParens("abc(d)(ef(g(h))ij)k)lm()o)p") === "abc(d)(ef(g(h))ij)klm()op")
+
+console.log(balanceParens("abc(d)e(fgh))(i)j)k") == "abc(d)e(fgh)(i)jk")
+console.log(balanceParens("abc((d)e(fgh)(i)j(k") == "abc(d)e(fgh)(i)jk")
+console.log(balanceParens("()") == "()")
+console.log(balanceParens("a(b)c)") == "a(b)c")
+console.log(balanceParens("(a)(bdd)c)") == "(a)(bdd)c")
+console.log(balanceParens("a(dbvb)c)") == "a(dbvb)c")
+console.log(balanceParens("a(b)(c)())") == "a(b)(c)()")
+console.log(balanceParens(")(") == "")
+console.log(balanceParens("(((((") == "")
+console.log(balanceParens(")(())(") == "(())")
+console.log(balanceParens("(()()(") == "()()")
+console.log(balanceParens(")())(()()(") == "()()()")
+
+// Challenge: nested parentheses...
+console.log(balanceParens("abc(d)(ef(g(h))ij)k)lm()o)p") == "abc(d)(ef(g(h))ij)klm()op")
+console.log(balanceParens(")(())(") == "(())")

c48509d778725bcb3008ad9412f3d48632035f46
Galuf1 <daniel.pizarro.farias@gmail.com>
2022-05-30 17:35:30 +0200

    solved python with stack

--- python/balance_parens.py
+++ python/balance_parens.py
@@ -1,2 +1,19 @@
+import re
 def balance_parens(str):
-    pass
+    strL = list(str)
+    stack = []
+    # print(strL)
+
+    for i in range(len(str)):
+        if str[i] == "(":
+            stack.append(i)
+        if str[i] == ")":
+            if len(stack) == 0:
+                strL[i] = ""
+            else:
+                stack.pop()
+    for j in stack:
+        strL[j] = ""
+    result= ''.join(strL)
+    return result
+    
